a038c4db6b0e3268528c2142aadb52fb879801bd
ATsarev01 <maxperov01@gmail.com>
2023-05-24 17:54:21 +0600

    initial commit

---
+++ src/main/java/com/hogwarts/schoolbd/SchoolBdApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.schoolbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBdApplication.class, args);
+    }
+
+}
